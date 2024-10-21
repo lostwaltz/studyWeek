45bb82b95b7a221c53951855e052befb8944a606
lostwaltz <lostwaltz89@gmail.com>
2024-10-22 03:45:38 +0900

    update questManager

--- Assets/QuestManager.cs
+++ Assets/QuestManager.cs
@@ -30,10 +30,6 @@ public class QuestManager : MonoBehaviour
 
             DontDestroyOnLoad(gameObject);
         }
-        else
-        {
-            Destroy(gameObject);
-        }
     }
 }
 
